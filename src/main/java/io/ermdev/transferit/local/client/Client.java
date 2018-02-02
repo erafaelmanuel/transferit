a6ec1ce5ae7a514223b4c6472ee50100114b87f6
erafaelmanuel <erafaelmanuel@gmail.com>
2018-02-02 23:57:15 +0800

    Update the server and client

--- src/main/java/io/ermdev/transferit/local/client/Client.java
+++ src/main/java/io/ermdev/transferit/local/client/Client.java
@@ -1,6 +1,11 @@
 package io.ermdev.transferit.local.client;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Client {
 
@@ -10,6 +15,25 @@ public class Client {
     public void findConnection() {
         try {
             Socket socket = new Socket("localhost", 23411);
+
+            File file = new File("sample.mp3");
+
+            System.out.println("File is exist : " + file.exists());
+
+            byte buffer[] = new byte[(int) file.length()];
+
+            System.out.println(buffer.length);
+
+            FileInputStream fis = new FileInputStream(file);
+            BufferedInputStream bis = new BufferedInputStream(fis);
+            int tae = bis.read(buffer, 0, buffer.length);
+
+            System.out.println(tae);
+
+            BufferedOutputStream writer = new BufferedOutputStream(socket.getOutputStream());
+            writer.write(buffer, 0, buffer.length);
+            writer.flush();
+            writer.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
--- src/main/java/io/ermdev/transferit/local/server/Server.java
+++ src/main/java/io/ermdev/transferit/local/server/Server.java
@@ -1,24 +1,45 @@
 package io.ermdev.transferit.local.server;
 
+import java.io.*;
 import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
 
 public class Server {
 
     private int port;
 
+    private ServerSocket serverSocket;
+    private Socket socket;
+
     public Server(int port) {
         this.port = port;
     }
-    private ServerSocket serverSocket;
 
     public void openConnection() {
         try {
             System.out.println("Waiting ...");
             serverSocket = new ServerSocket(port);
-            serverSocket.accept();
+            socket = serverSocket.accept();
             System.out.println("Connected!");
+            receivedFile();
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    public void receivedFile() throws Exception {
+        int n;
+        byte buffer[] = new byte[4303444];
+        File file = new File("produce.mp3");
+        FileOutputStream fos = new FileOutputStream(file);
+        BufferedOutputStream bos = new BufferedOutputStream(fos);
+        BufferedInputStream bis = new BufferedInputStream(socket.getInputStream());
+
+        while((n = bis.read(buffer, 0, buffer.length)) != 1) {
+            bos.write(buffer, 0, n);
+        }
+        bos.flush();
+        bos.close();
     }
 }

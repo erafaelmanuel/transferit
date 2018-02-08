7335a39f645aaa51bee574308ba2f37677014e6f
erafaelmanuel <erafaelmanuel@gmail.com>
2018-02-08 21:49:27 +0800

    Update client_ui.fxml

--- src/main/java/io/ermdev/transferit/ui/controller/ClientUIController.java
+++ src/main/java/io/ermdev/transferit/ui/controller/ClientUIController.java
@@ -1,16 +1,20 @@
 package io.ermdev.transferit.ui.controller;
 
 import io.ermdev.transferit.local.client.Client;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.stage.FileChooser;
 
 import javax.swing.*;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ClientUIController {
 
     private Client client;
+    private final List<File> FILES = new ArrayList<>();
 
     @FXML
     private TextField txHost;
@@ -18,7 +22,7 @@ public class ClientUIController {
     private Button btnCDC;
 
     @FXML
-    public void onActionConnect(ActionEvent event) {
+    public void onActionConnect() {
         try {
             client = new Client(txHost.getText(), 23411);
             if (btnCDC.getText().equals("Connect")) {
@@ -28,6 +32,30 @@ public class ClientUIController {
             }
         } catch (Exception e) {
             JOptionPane.showMessageDialog(null, "Unable to connect.");
+        }
+    }
+
+    @FXML
+    public void onActionSend() {
+        if (client != null && FILES.size() > 0) {
+            for (File file : FILES) {
+                try {
+                    client.send(file);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+            FILES.clear();
+        }
+    }
+
+    @FXML
+    public void onActionBrowse() {
+        FileChooser fileChooser = new FileChooser();
+        File file = fileChooser.showOpenDialog(null);
+        if(file != null) {
+            FILES.add(file);
+            System.out.println(file.getName());
         }
     }
 }

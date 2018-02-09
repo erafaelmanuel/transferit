81b522416e4a9ecfe0cf39492d27a229ed475ae9
erafaelmanuel <erafaelmanuel@gmail.com>
2018-02-09 22:51:32 +0800

    Update ClientUIController

--- src/main/java/io/ermdev/transferit/ui/controller/ClientUIController.java
+++ src/main/java/io/ermdev/transferit/ui/controller/ClientUIController.java
@@ -2,20 +2,26 @@ package io.ermdev.transferit.ui.controller;
 
 import io.ermdev.transferit.local.client.BasicClient;
 import io.ermdev.transferit.local.client.Receiver;
+import io.ermdev.transferit.util.Subscriber;
+import javafx.application.Platform;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.stage.FileChooser;
 
 import javax.swing.*;
 import java.io.File;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.ResourceBundle;
 
-public class ClientUIController {
+public class ClientUIController implements Subscriber {
 
     private BasicClient client;
-    private final List<File> FILES = new ArrayList<>();
+    private List<File> files = new ArrayList<>();
+    private Receiver receiver = new Receiver();
 
     @FXML
     private TextField txHost;
@@ -25,15 +31,14 @@ public class ClientUIController {
     @FXML
     public void onActionConnect() {
         try {
-            Receiver receiver = new Receiver();
-            receiver.setHost(txHost.getText());
-            receiver.setPort(23411);
-
-            client = new BasicClient(receiver);
             if (btnCDC.getText().equals("Connect")) {
-                btnCDC.setText("Disconnect");
+                receiver.subscribe(this);
+                receiver.setHost(txHost.getText());
+                receiver.setPort(23411);
+                receiver.setConnected(true);
+                client = new BasicClient(receiver);
             } else {
-                btnCDC.setText("Connect");
+                receiver.setConnected(false);
             }
         } catch (Exception e) {
             JOptionPane.showMessageDialog(null, "Unable to connect.");
@@ -42,17 +47,19 @@ public class ClientUIController {
 
     @FXML
     public void onActionSend() {
-        Thread thread = new Thread(()->{
-            if (client != null && FILES.size() > 0) {
-                for (File file : FILES) {
+        Thread thread = new Thread(() -> {
+            if (client != null && files.size() > 0) {
+                for (File file : files) {
                     try {
                         client.openTransaction(file);
                         System.out.println(file.getName());
                     } catch (Exception e) {
                         e.printStackTrace();
+                        files.clear();
+                        break;
                     }
                 }
-                FILES.clear();
+                files.clear();
             }
         });
         thread.start();
@@ -61,9 +68,24 @@ public class ClientUIController {
     @FXML
     public void onActionBrowse() {
         FileChooser fileChooser = new FileChooser();
-        List<File> files = fileChooser.showOpenMultipleDialog(null);
-        if (files != null && files.size() > 0) {
-            FILES.addAll(files);
+        List<File> newFiles = fileChooser.showOpenMultipleDialog(null);
+        if (newFiles != null && newFiles.size() > 0) {
+            files.addAll(newFiles);
         }
+
+    }
+
+    @Override
+    public void update(boolean status) {
+        Thread thread = new Thread(() -> {
+            Platform.runLater(() -> {
+                if (status) {
+                    btnCDC.setText("Disconnect");
+                } else {
+                    btnCDC.setText("Connect");
+                }
+            });
+        });
+        thread.start();
     }
 }

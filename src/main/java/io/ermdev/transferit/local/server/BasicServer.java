d856ce9c6241f58ff163896496c46f31db830b61
erafaelmanuel <erafaelmanuel@gmail.com>
2018-02-09 14:24:04 +0800

    Create BasicServer

---
+++ src/main/java/io/ermdev/transferit/local/server/BasicServer.java
@@ -0,0 +1,66 @@
+package io.ermdev.transferit.local.server;
+
+import io.ermdev.transferit.local.Signature;
+
+import java.io.BufferedInputStream;
+import java.io.DataInputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
+public class BasicServer {
+
+    private int port;
+    private ServerSocket serverSocket;
+    private Signature sender;
+
+    public BasicServer(int port) {
+        this.port = port;
+    }
+
+    public void channeling() throws Exception {
+        serverSocket = new ServerSocket(port);
+        while (true) {
+            try {
+                System.out.println("Waiting ...");
+                Socket socket = serverSocket.accept();
+
+                if (sender == null || !sender.isEnabled()) {
+                    System.out.println("Someone want to connect!");
+                    System.out.println("Accept? (y/n) ");
+
+                    if (new Scanner(System.in).next().equalsIgnoreCase("y")) {
+                        System.out.println(socket.getInetAddress().getCanonicalHostName());
+                        System.out.println(socket.getInetAddress().getHostAddress());
+                        System.out.println(socket.getInetAddress().getHostName());
+                        sender = new Signature(socket.getInetAddress().getHostAddress(), true);
+                    }
+                }
+                if (sender != null && sender.isEnabled()) {
+                    openSession(socket);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void openSession(Socket socket) throws Exception {
+        if (socket != null && !socket.isClosed()) {
+            receivedFile(socket);
+            if (!socket.isClosed())
+                socket.close();
+        }
+    }
+
+    private void receivedFile(Socket socket) throws Exception {
+        BufferedInputStream bis = new BufferedInputStream(socket.getInputStream());
+        DataInputStream dis = new DataInputStream(bis);
+
+        String fileName = dis.readUTF();
+        Files.copy(dis, Paths.get(fileName));
+        dis.close();
+    }
+}

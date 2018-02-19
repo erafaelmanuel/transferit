e2d862c1a30bbca1714d1a0068d9c4b29014be0e
erafaelmanuel <erafaelmanuel@gmail.com>
2018-02-19 23:08:08 +0800

    Update client_flatui.fxml

--- src/main/java/io/ermdev/transferit/ui/client/ClientController.java
+++ src/main/java/io/ermdev/transferit/ui/client/ClientController.java
@@ -1,9 +1,11 @@
 package io.ermdev.transferit.ui.client;
 
+import com.jfoenix.controls.JFXButton;
 import io.ermdev.transferit.ui.stage.WelcomeStage;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
@@ -18,6 +20,12 @@ public class ClientController implements Initializable {
 
     @FXML private ImageView imgvback;
 
+    @FXML private JFXButton btnConnect;
+
+    @FXML private JFXButton btnSendFile;
+
+    @FXML private Label lblStatus;
+
     private WelcomeStage welcomeStage;
 
     @Override
@@ -26,17 +34,37 @@ public class ClientController implements Initializable {
         imgvback.setImage(new Image(getClass().getResource("/image/img_prev.png").toString()));
     }
 
+    public void setWelcomeStage(WelcomeStage welcomeStage) {
+        this.welcomeStage = welcomeStage;
+    }
+
     @FXML
     protected void onMousePressedBack(MouseEvent event) {
         Stage stage = (Stage) ((Node) (event.getSource())).getScene().getWindow();
         stage.close();
 
-        if(welcomeStage != null) {
+        if (welcomeStage != null) {
             welcomeStage.show();
         }
     }
 
-    public void setWelcomeStage(WelcomeStage welcomeStage) {
-        this.welcomeStage = welcomeStage;
+    private boolean s = true;
+
+    @FXML
+    protected void onActionConnect() {
+        if(s) {
+            lblStatus.setVisible(true);
+            btnConnect.setText("Disconnect");
+            btnConnect.setStyle("-fx-background-color:#ff7675");
+            btnSendFile.setDisable(false);
+            s = false;
+        } else {
+            lblStatus.setText("Waiting ...");
+            lblStatus.setStyle("-fx-text-fill:#000000");
+            btnConnect.setText("Connect");
+            btnConnect.setStyle("-fx-background-color:#00b894");
+            btnSendFile.setDisable(true);
+            s = true;
+        }
     }
 }

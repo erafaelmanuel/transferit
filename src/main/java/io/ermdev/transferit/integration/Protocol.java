3e35c92a1aae37adfe6d46b2d16d45f793098dac
erafaelmanuel <erafaelmanuel@gmail.com>
2018-02-21 03:23:51 +0800

    Update Protocol

--- src/main/java/io/ermdev/transferit/integration/Protocol.java
+++ src/main/java/io/ermdev/transferit/integration/Protocol.java
@@ -1,10 +1,14 @@
 package io.ermdev.transferit.integration;
 
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.Socket;
 
 public class Protocol {
 
     private Socket socket;
+
+    public Protocol() {}
 
     public boolean isAlive() {
         if (socket == null) {
@@ -38,9 +42,23 @@ public class Protocol {
             if (isAlive()) {
                 this.socket.close();
             }
-        } catch (Exception e) {
-            this.socket = null;
-        }
+        } catch (Exception e) {}
         this.socket = socket;
+    }
+
+    public InputStream getInputStream() throws Exception {
+        if (isAlive()) {
+            return socket.getInputStream();
+        } else {
+            throw new RuntimeException("Socket is not available");
+        }
+    }
+
+    public OutputStream getOutputStream() throws Exception {
+        if (isAlive()) {
+            return socket.getOutputStream();
+        } else {
+            throw new RuntimeException("Socket is not available");
+        }
     }
 }

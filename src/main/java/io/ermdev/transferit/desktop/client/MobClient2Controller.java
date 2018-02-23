467e9884b4e3f53371e5e27184b4c8823b308786
erafaelmanuel <erafaelmanuel@gmail.com>
2018-02-23 14:03:21 +0800

    Update MobClient2

--- src/main/java/io/ermdev/transferit/desktop/client/MobClient2Controller.java
+++ src/main/java/io/ermdev/transferit/desktop/client/MobClient2Controller.java
@@ -8,6 +8,9 @@ import io.ermdev.transferit.integration.TcpClient;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.input.DragEvent;
+import javafx.scene.input.Dragboard;
+import javafx.scene.input.TransferMode;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.FileChooser;
@@ -70,7 +73,7 @@ public class MobClient2Controller implements ClientListener {
             if (client != null) {
                 itemManager.setItems(items);
                 Platform.runLater(() -> {
-                    lblStatus.setStyle("-fx-background-color: #fdcb6e");
+                    lblStatus.setStyle("-fx-background-color: #0984e3");
                     lblStatus.setText("Sending");
                 });
                 for (Item item : items) {
@@ -84,6 +87,33 @@ public class MobClient2Controller implements ClientListener {
             }
         });
         thread.start();
+    }
+
+    @FXML void onDrag(DragEvent event) {
+        if (event.getDragboard().hasFiles()) {
+            event.acceptTransferModes(TransferMode.COPY_OR_MOVE);
+        }
+        event.consume();
+    }
+
+    @FXML void onDrop(DragEvent event) {
+        Dragboard db = event.getDragboard();
+        boolean success = false;
+        if (db.hasFiles()) {
+            container.getChildren().clear();
+            long size = 0;
+            for (File file : db.getFiles()) {
+                Item item = new Item(file);
+                size += file.length();
+                container.getChildren().add(new MyBox(item));
+                items.add(item);
+            }
+            lblStatus.setStyle("-fx-background-color: #ff9f43");
+            lblStatus.setText(items.size() + " file(s)   --  Total : " + TrafficUtil.size(size));
+            success = true;
+        }
+        event.setDropCompleted(success);
+        event.consume();
     }
 
     @Override

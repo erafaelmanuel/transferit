d261070e40fa94e059f3fd09dafb22896b0bd2c1
erafaelmanuel <erafaelmanuel@gmail.com>
2018-02-26 23:50:12 +0800

    Optimize the import packages of ItemSubscriber

--- src/main/java/io/ermdev/transferit/integration/ItemSubscriber.java
+++ src/main/java/io/ermdev/transferit/integration/ItemSubscriber.java
@@ -1,7 +1,5 @@
 package io.ermdev.transferit.integration;
 
-import io.ermdev.transferit.integration.Subscriber;
-
 public interface ItemSubscriber extends Subscriber {
 
     void beforeItemRelease();

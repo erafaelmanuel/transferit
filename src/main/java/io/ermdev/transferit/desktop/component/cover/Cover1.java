20bf2ce5717647e7b2205153e4576859e47d81be
erafaelmanuel <erafaelmanuel@gmail.com>
2018-03-03 03:58:15 +0800

    Update

--- src/main/java/io/ermdev/transferit/desktop/component/cover/Cover1.java
+++ src/main/java/io/ermdev/transferit/desktop/component/cover/Cover1.java
@@ -5,7 +5,7 @@ import io.ermdev.transferit.desktop.component.Cover;
 public class Cover1 extends Cover {
 
     public Cover1() {
-        setBackgroundColor("#e58e26");
+        setBackgroundColor("#cc8e35");
         setImage("/image/cover/kangaroo.png");
         setLabelText("Transfer files between your devices");
         setLabelColor("#543e11");

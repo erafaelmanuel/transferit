190679ceaa8fad0ab81bf9fc2f3c021e0c0e34fe
erafaelmanuel <erafaelmanuel@gmail.com>
2018-03-04 04:18:11 +0800

    Update

---
+++ src/main/java/io/ermdev/transferit/desktop/welcome/WelcomeInteract.java
@@ -0,0 +1,4 @@
+package io.ermdev.transferit.desktop.welcome;
+
+public interface WelcomeInteract {
+}
--- src/main/java/io/ermdev/transferit/desktop/welcome/WelcomeListener.java
+++
@@ -1,6 +0,0 @@
-package io.ermdev.transferit.desktop.welcome;
-
-@FunctionalInterface
-public interface WelcomeListener {
-    void onClose(boolean isServer);
-}

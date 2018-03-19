8078f8d7986bf93b9354a3aea578085100caf7a3
erafaelmanuel <erafaelmanuel@gmail.com>
2018-03-19 21:11:49 +0800

    Create stage and controller for settings

---
+++ src/main/java/io/ermdev/transferit/desktop/option/SettingController.java
@@ -0,0 +1,4 @@
+package io.ermdev.transferit.desktop.option;
+
+public class SettingController {
+}
---
+++ src/main/java/io/ermdev/transferit/desktop/option/SettingStage.java
@@ -0,0 +1,49 @@
+package io.ermdev.transferit.desktop.option;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.net.URL;
+
+final public class SettingStage extends Stage {
+
+    private SettingController sdc;
+
+    public SettingStage() {
+        try {
+            final String FXML = "/fxml/settings.fxml";
+            //final String CSS = "/css/settings-style.css";
+            final FXMLLoader loader = new FXMLLoader();
+            final URL location = getClass().getResource(FXML);
+            //final String style = getClass().getResource(CSS).toExternalForm();
+            loader.setLocation(location);
+
+            final Parent root = loader.load();
+            final Scene scene = new Scene(root, 247, 400);
+            //scene.getStylesheets().add(style);
+
+            setTitle("Transferit v1.0");
+            setScene(scene);
+            setController(loader.getController());
+            setOnCloseRequest(e -> System.exit(0));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void display() {
+        setResizable(false);
+        sizeToScene();
+        show();
+    }
+
+    public SettingController getController() {
+        return sdc;
+    }
+
+    public void setController(SettingController sdc) {
+        this.sdc = sdc;
+    }
+}

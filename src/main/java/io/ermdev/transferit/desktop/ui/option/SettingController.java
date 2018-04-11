774530c3dd812077724b3dea0277c82830a373e8
erafaelmanuel <erafaelmanuel@gmail.com>
2018-04-12 04:22:43 +0800

    Update

--- src/main/java/io/ermdev/transferit/desktop/ui/option/SettingController.java
+++ src/main/java/io/ermdev/transferit/desktop/ui/option/SettingController.java
@@ -1,14 +1,12 @@
 package io.ermdev.transferit.desktop.ui.option;
 
-import com.jfoenix.controls.JFXButton;
 import io.ermdev.transferit.desktop.util.MasterConfig;
-import javafx.animation.TranslateTransition;
-import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.DirectoryChooser;
 import javafx.stage.Stage;
 
@@ -31,11 +29,6 @@ public class SettingController implements Initializable {
     @FXML
     TextField txIP;
 
-    @FXML
-    JFXButton btnBrowse;
-
-    private volatile boolean isFocus = false;
-
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         txDir.setText(new File(masterConfig.getDirOrDefault()).getAbsolutePath());
@@ -44,30 +37,10 @@ public class SettingController implements Initializable {
             txIP.setText(Inet4Address.getLocalHost().getHostAddress());
         } catch (UnknownHostException e) {
         }
-        btnBrowse.setVisible(false);
-        txDir.focusedProperty().addListener((a, b, c) -> {
-            if (c) {
-                isFocus = true;
-                btnBrowse.setVisible(true);
-            } else {
-                isFocus = false;
-                Thread thread = new Thread(()-> {
-                    try {
-                        Thread.sleep(800);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                    if (!isFocus) {
-                        btnBrowse.setVisible(false);
-                    }
-                });
-                thread.start();
-            }
-        });
     }
 
     @FXML
-    void onBrowse(ActionEvent event) {
+    void onBrowse(MouseEvent event) {
         final Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         DirectoryChooser chooser = new DirectoryChooser();
         chooser.setTitle("Save to");
